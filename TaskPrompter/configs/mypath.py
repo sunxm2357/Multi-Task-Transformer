41a400b58b9149613365551ddeb45bab3b070257
Ximeng Sun <sunxm@ximengs-mini.mynetworksettings.com>
2023-11-04 21:31:47 -0400

    update for scc

--- TaskPrompter/configs/mypath.py
+++ TaskPrompter/configs/mypath.py
@@ -1,7 +1,7 @@
 import os
 PROJECT_ROOT_DIR = os.path.dirname(os.path.abspath(__file__)).split('/')[0]
 
-db_root = 'your_dataset_directory'
+db_root = '/projectnb/ivc-ml/sunxm/datasets'
 
 db_names = {'PASCALContext': 'PASCALContext', 'NYUD_MT': 'NYUDv2', 'Cityscapes3D': 'Cityscapes'}
 db_paths = {}
