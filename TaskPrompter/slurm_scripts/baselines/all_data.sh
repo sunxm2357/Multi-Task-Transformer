26c1bed093973b7449e47d07384673a15f53c3cf
Ximeng Sun <sunxm@ximengs-mini.mynetworksettings.com>
2023-11-06 12:59:06 -0500

    update for scc

---
+++ TaskPrompter/slurm_scripts/baselines/all_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -A m4277
+#SBATCH -C gpu
+#SBATCH -q shared
+#SBATCH -t 24:00:00
+#SBATCH -n 1
+#SBATCH -c 8
+#SBATCH --gpus-per-task=1
+
+export SLURM_CPU_BIND="cores"
+
+conda activate sunxm
+module load pytorch
+
+cd /pscratch/sd/h/hwchen/code/Multi-Task-Transformer/TaskPrompter
+
+python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/nyud_vitLp16_taskprompter_no_overlap.yml' --run_mode train  #--trained_model  pretrained_mode0l_path
