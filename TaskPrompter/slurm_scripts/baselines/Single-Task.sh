28f6201db32973969075e2d92c6970011bc1667d
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-11-06 17:00:39 -0500

    add NYUD create init

---
+++ TaskPrompter/slurm_scripts/baselines/Single-Task.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -A m4277
+#SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -t 23:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=4
+#SBATCH -c 32
+#SBATCH --gpus-per-task=1
+#SBATCH -J single_task
+
+
+export SLURM_CPU_BIND="cores"
+
+conda activate sunxm
+module load pytorch
+
+cd /pscratch/sd/h/hwchen/code/Multi-Task-Transformer/TaskPrompter
+
+# semseg
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/nyud_vitLp16_taskprompter_semseg.yml' --run_mode train
+
+# edge
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/nyud_vitLp16_taskprompter_edge.yml' --run_mode train
+
+# depth
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/nyud_vitLp16_taskprompter_depth.yml' --run_mode train
+
+# norm
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/nyud_vitLp16_taskprompter_normal.yml' --run_mode train
