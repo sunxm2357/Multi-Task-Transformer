ceace269f00613d8f1d0c411e36205586dfac8f3
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-11-07 11:10:54 -0500

    add init

--- TaskPrompter/configs/mypath.py
+++ TaskPrompter/configs/mypath.py
@@ -1,7 +1,8 @@
 import os
 PROJECT_ROOT_DIR = os.path.dirname(os.path.abspath(__file__)).split('/')[0]
 
-db_root = '/pscratch/sd/h/hwchen/datasets'
+# db_root = '/pscratch/sd/h/hwchen/datasets'
+db_root = '/projectnb/ivc-ml/sunxm/datasets'
 
 db_names = {'PASCALContext': 'PASCALContext', 'NYUD_MT': 'NYUDv2', 'Cityscapes3D': 'Cityscapes'}
 db_paths = {}
