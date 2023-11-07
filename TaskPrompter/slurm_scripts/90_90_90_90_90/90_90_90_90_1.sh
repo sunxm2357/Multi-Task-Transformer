eaa02d777ddb7e03cd3da8378872457fb275d036
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-11-07 10:53:36 -0500

    add NYUD create init

---
+++ TaskPrompter/slurm_scripts/90_90_90_90_90/90_90_90_90_1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -A m4277
+#SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -t 23:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=4
+#SBATCH -c 32
+#SBATCH --gpus-per-task=1
+#SBATCH -J 90_90_90_90_0_3
+
+
+export SLURM_CPU_BIND="cores"
+
+conda activate sunxm
+module load pytorch
+
+cd /pscratch/sd/h/hwchen/code/Multi-Task-Transformer/TaskPrompter
+
+# semseg
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp0.yml' --run_mode train &
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp1.yml' --run_mode train &
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp2.yml' --run_mode train &
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp3.yml' --run_mode train &
+wait
---
+++ TaskPrompter/slurm_scripts/90_90_90_90_90/90_90_90_90_2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -A m4277
+#SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -t 23:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=4
+#SBATCH -c 32
+#SBATCH --gpus-per-task=1
+#SBATCH -J 90_90_90_90_4_7
+
+
+export SLURM_CPU_BIND="cores"
+
+conda activate sunxm
+module load pytorch
+
+cd /pscratch/sd/h/hwchen/code/Multi-Task-Transformer/TaskPrompter
+
+# semseg
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_90/exp4.yml' --run_mode train &
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_90/exp5.yml' --run_mode train &
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_90/exp6.yml' --run_mode train &
+srun --ntasks=1 python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_90/exp7.yml' --run_mode train &
+wait
