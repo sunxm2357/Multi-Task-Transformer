174e15b8c2554edcfd458394b183dbb955a3ba3c
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-11-22 22:17:40 -0500

    add task metric

--- TaskPrompter/main_non_overlap_data_affinity.py
+++ TaskPrompter/main_non_overlap_data_affinity.py
@@ -41,6 +41,8 @@ parser.add_argument('--run_mode',
                     help='Config file for the experiment')
 parser.add_argument('--trained_model', default=None,
                     help='Config file for the experiment')
+parser.add_argument('--affinity_freq', default=10, type=int,
+                    help='Config file for the experiment')
 args = parser.parse_args()
 
 print('local rank: %s' %args.local_rank)
