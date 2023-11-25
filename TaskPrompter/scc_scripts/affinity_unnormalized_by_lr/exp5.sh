0ac27ac8449f7adea3b8f3e56c6527c9e56d20e7
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-11-25 15:32:07 -0500

    add common affinity setting

---
+++ TaskPrompter/scc_scripts/affinity_unnormalized_by_lr/exp5.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -l
+
+#$ -N affinity_unnormalized_by_lr_5
+
+#$ -m bea
+
+#$ -M sunxm@bu.edu
+
+# Set SCC project
+#$ -P ivc-ml
+
+# Request my job to run on Buy-in Compute group hardware my_project has access to
+#$ -l buyin
+
+# Request 4 CPUs
+#$ -pe omp 3
+
+# Request 2 GPU
+#$ -l gpus=1
+
+# Specify the minimum GPU compute capability
+#$ -l gpu_c=7.5
+
+#$  -l gpu_memory=48G
+
+#$ -l h_rt=48:00:00
+
+# Keep track of information related to the current job
+echo "=========================================================="
+echo "Start date : $(date)"
+echo "Job name : $JOB_NAME"
+echo "Job ID : $JOB_ID  $SGE_TASK_ID"
+echo "=========================================================="
+
+
+
+module load miniconda
+module load cuda/11.1
+module load gcc
+conda activate taskprompter
+#conda install -c conda-forge opencv
+
+cd /projectnb/ivc-ml/sunxm/code/Multi-Task-Transformer/TaskPrompter
+
+python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data_affinity.py --config_exp './configs/nyud/affinity_unnormalized_by_lr/affinity_5.yml' --run_mode train  --affinity_normalized_by_stl
