222a669480c94c870022d45e9f859dff0d1a0c2c
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-11-27 13:36:02 -0500

    add time counting to the code

--- TaskPrompter/main_non_overlap_data_affinity.py
+++ TaskPrompter/main_non_overlap_data_affinity.py
@@ -184,6 +184,8 @@ def main():
                 print('Epoch %d/%d' %(epoch+1, p['epochs']))
                 print('-'*10)
 
+            import pdb
+            pdb.set_trace()
             end_signal, iter_count, aff_mat = train_phase_no_overlap_data_affinity(p, args, train_dataloaders, affinity_dataloaders, test_dataloader, model, criterion, optimizer, scheduler, epoch, tb_writer_train, tb_writer_test, iter_count, aff_mat)
 
             if end_signal:
