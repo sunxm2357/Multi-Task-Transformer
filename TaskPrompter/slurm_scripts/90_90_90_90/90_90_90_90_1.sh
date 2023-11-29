4dd4810d899ad92e8babf6e7799578f785d40a84
Ximeng Sun <sunxm@ximengs-mini.mynetworksettings.com>
2023-11-28 22:09:58 -0500

    add equal image same

--- TaskPrompter/slurm_scripts/90_90_90_90/90_90_90_90_1.sh
+++ TaskPrompter/slurm_scripts/90_90_90_90/90_90_90_90_1.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH -c 32
 #SBATCH --gpus-per-task=1
-#SBATCH -J 90_90_90_90_0_3
+#SBATCH -J 90_90_90_90_same_0_3
 #SBATCH --exclusive
 #SBATCH --output=%x_%j.out
 
@@ -17,8 +17,8 @@ export SLURM_CPU_BIND="cores"
 cd /pscratch/sd/h/hwchen/code/Multi-Task-Transformer/TaskPrompter
 
 # semseg
-srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp0.yml' --run_mode train &
-srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp1.yml' --run_mode train &
-srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp2.yml' --run_mode train &
-srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp3.yml' --run_mode train &
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_same/exp0.yml' --run_mode train &
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_same/exp1.yml' --run_mode train &
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_same/exp2.yml' --run_mode train &
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_same/exp3.yml' --run_mode train &
 wait
---
+++ TaskPrompter/slurm_scripts/90_90_90_90_same/90_90_90_90_1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -A m4277
+#SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -t 23:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=4
+#SBATCH -c 32
+#SBATCH --gpus-per-task=1
+#SBATCH -J 90_90_90_90_0_3
+#SBATCH --exclusive
+#SBATCH --output=%x_%j.out
+
+export SLURM_CPU_BIND="cores"
+
+
+cd /pscratch/sd/h/hwchen/code/Multi-Task-Transformer/TaskPrompter
+
+# semseg
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp0.yml' --run_mode train &
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp1.yml' --run_mode train &
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp2.yml' --run_mode train &
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp3.yml' --run_mode train &
+wait
