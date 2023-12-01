4d9feda41ccec839f26d6ad3e8b554c8965863f6
Ximeng Sun <sunxm@ximengs-mini.mynetworksettings.com>
2023-12-01 12:38:02 -0500

    add equal image same

--- TaskPrompter/slurm_scripts/90_90_90_90_same/90_90_90_90_1.sh
+++ TaskPrompter/slurm_scripts/90_90_90_90_same/90_90_90_90_1.sh
@@ -17,8 +17,8 @@ export SLURM_CPU_BIND="cores"
 cd /pscratch/sd/h/hwchen/code/Multi-Task-Transformer/TaskPrompter
 
 # semseg
-srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp0.yml' --run_mode train &
-srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp1.yml' --run_mode train &
-srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp2.yml' --run_mode train &
-srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90/exp3.yml' --run_mode train &
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_same/exp0.yml' --run_mode train &
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_same/exp1.yml' --run_mode train &
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_same/exp2.yml' --run_mode train &
+srun --exact -u -n 1 --gpus-per-task 1 -c 32 --mem-per-gpu=55G python -m torch.distributed.launch --nproc_per_node=1  --master_port=$((RANDOM%1000+12000))  main_non_overlap_data.py --config_exp './configs/nyud/90_90_90_90_same/exp3.yml' --run_mode train &
 wait
