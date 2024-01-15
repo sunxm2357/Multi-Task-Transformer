015025f84a1dc4d22685aef87c54e08c67f94b46
Ximeng Sun <sunxm@ximengs-mini.mynetworksettings.com>
2024-01-14 22:15:01 -0500

    add cityscapes

--- TaskPrompter/data/transforms.py
+++ TaskPrompter/data/transforms.py
@@ -245,6 +245,8 @@ class Normalize:
         self.std = np.array(std, dtype=np.float32).reshape(1, 1, 3)
 
     def normalize_img(self, img):
+        import pdb
+        pdb.set_trace()
         assert img.dtype == np.float32
         scaled = img.copy() / 255.
         scaled -= self.mean
